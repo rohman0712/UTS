a81320622673c8138fe988ae99b842cc164f2e43
ade <adeyds@gmail.com>
2022-05-08 14:08:57 +0700

    fix sepedaMotor

--- UTS/kendaraan/SepedaMotor.java
+++ UTS/kendaraan/SepedaMotor.java
@@ -1,10 +1,23 @@
 package kendaraan;
+
+import mesin.Mesin;
+
 public class SepedaMotor
 {
 	private String bahan_bakar;
-	private String mesin;
+	private Mesin mesin;
 	private String transmisi;
-	
+
+	private String nama;
+
+	public void setNama(String nama){
+		this.nama = nama;
+	}
+
+	public String getNama(){
+		return this.nama;
+	}
+
 	public void setBahan_bakar(String bahan_bakar)
 	{
 		this.bahan_bakar=bahan_bakar;
@@ -13,11 +26,11 @@ public class SepedaMotor
 	{
 		return this.bahan_bakar;
 	}
-	public void setMesin(String mesin)
+	public void setMesin(Mesin mesin)
 	{
 		this.mesin=mesin;
 	}
-	public String getMesin()
+	public Mesin getMesin()
 	{
 		return this.mesin;
 	}
